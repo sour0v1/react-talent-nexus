6bb078acdaf553646c200d26b3042d4d9d787a0d
Sourov <sourovdas730@gmail.com>
2024-03-22 20:51:05 +0600

    set up and added header, footer

---
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppliedJobs = () => {
+    return (
+        <div>
+            <h4>Jobs have Applied</h4>
+        </div>
+    );
+};
+
+export default AppliedJobs;
